20e89059aa7d39a5dc29318a04579eb229982176
Josef8852 <josefaboshady2@gmail.com>
2026-04-11 22:07:37 +0200

    SideNav as client component -> highlight current side navigation

--- app/_components/SideNav.tsx
+++ app/_components/SideNav.tsx
@@ -1,9 +1,13 @@
+"use client"
+
 import {
   CalendarDaysIcon,
   HomeIcon,
   UserIcon,
 } from '@heroicons/react/24/solid';
 import SignOutButton from './SignOutButton';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const navLinks = [
   {
@@ -23,19 +27,27 @@ const navLinks = [
   },
 ];
 
-const  SideNav:React.FC = () => {
+const SideNav: React.FC = () => {
+  
+  const pathname = usePathname();
+  
+  
   return (
     <nav className='border-r border-primary-900'>
       <ul className='flex flex-col gap-2 h-full text-lg'>
         {navLinks.map((link) => (
           <li key={link.name}>
-            <a
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+            <Link
+              className={`
+                py-3 px-5 hover:bg-primary-900
+                hover:text-primary-100 transition-colors
+                flex items-center gap-4
+                font-semibold text-primary-200  ${pathname === link.href ? "bg-primary-900" : ""} `}
               href={link.href}
             >
               {link.icon}
               <span>{link.name}</span>
-            </a>
+            </Link>
           </li>
         ))}
 
